80174c58bc4105c2a71bf071ae5a4ea16d482a48
edwardmercado <edwardallenmercado@gmail.com>
2021-03-16 21:56:28 +0800

    Add CDN Stack

---
+++ cdn-stack/commands.sh
@@ -0,0 +1,26 @@
+# validate cfn
+#aws cloudformation validate-template --template-body ./template.yaml --region ap-southeast-1 --profile apper
+
+# package cfn
+aws cloudformation package --template ./template.yaml --s3-bucket challenge2-bucket --output json > packaged-template.json --region ap-southeast-1 --profile apper
+
+# deploy cfn
+aws cloudformation deploy --template ./packaged-template.json --stack-name emercado-tier-2-cdn-stack  --capabilities CAPABILITY_IAM --region ap-southeast-1 --profile apper
+
+# #Pub Sub 1
+# 10.0.0.0/19
+
+# #Pub Sub 2
+# 10.0.32.0/19
+
+# #Priv Sub 1
+# 10.0.64.0/19
+
+# #Priv Sub 2
+# 10.0.96.0/19
+
+# #DB Sub 1
+# 10.0.128.0/19
+
+# #DB Sub 2
+# 10.0.160.0/19
